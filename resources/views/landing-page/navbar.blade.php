50a22df3ca53d6099a6a9d6ad1ea491af6ed938c
filzaharry <harryfilza@gmail.com>
2024-01-25 13:50:36 +0700

    navbar desktop layouting

--- resources/views/landing-page/navbar.blade.php
+++ resources/views/landing-page/navbar.blade.php
@@ -1,5 +1,19 @@
 {{-- Navbar --}}
 <div class="ribbon-info">
-    <p>Selected clinic OPEN ON SUNDAY . Book Appointment Now</p>
+    <p>Selected clinic <strong style="font-family: 'PoppinsMedium' !important">OPEN ON SUNDAY</strong>. Book Appointment Now</p>
+</div>
+<div class="nav-row">
+    <div class="nav-container-logo">
+        <img class="nav-logo" src="{{asset('img/nav-logo.png')}}" alt="nav-logo">
+        <p class="nav-logo-text">Praktik Mandiri Dokter Asep</p>
+    </div>
+    <div class="nav-menu">
+        <p class="nav-item">Home</p>
+        <p class="nav-item">Konsultasi</p>
+        <p class="nav-item">Tentang</p>
+    </div>
+    <div class="nav-button">
+        <button class="nav-btn-login">Login</button>
+    </div>
 </div>
 {{-- End Navbar --}}
